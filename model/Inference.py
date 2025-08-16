96b7c06fd73a47295d4183d3aeafad489450492d
WANG1911 <150334577+WANG1911@users.noreply.github.com>
2025-08-16 10:12:07 +0800

    Add files via upload

---
+++ model/Inference.py
@@ -0,0 +1,26 @@
+import os
+import torch
+from PIL import Image
+import pandas as pd
+from model import CustomTNT
+from config import Config
+from utils.transforms import val_transform
+
+def predict_and_save(model, img_folder, transform, device, output_folder):
+    model.eval()
+    with torch.no_grad():
+        for img_name in os.listdir(img_folder):
+            prefix = img_name.split('.')[0]
+            img_path = os.path.join(img_folder, img_name)
+            img = Image.open(img_path).convert("RGB")
+            img = transform(img).unsqueeze(0).to(device)
+            output = model(img)
+            output_matrix = output.cpu().numpy().reshape(*Config.OUTPUT_SIZE)
+            output_csv_path = os.path.join(output_folder, prefix + ".csv")
+            pd.DataFrame(output_matrix).to_csv(output_csv_path, index=False, header=False)
+
+if __name__ == '__main__':
+    device = torch.device(Config.DEVICE)
+    model = CustomTNT().to(device)
+    model.load_state_dict(torch.load(Config.BEST_MODEL_SAVE_PATH))
+    predict_and_save(model, Config.TEST_IMG_FOLDER, val_transform, device, Config.OUTPUT_FOLDER)
